5db4576be703367a8d7eafd00cb2989a3d90c7c2
FLEURYESTEBAN <128791669+FLEURYESTEBAN@users.noreply.github.com>
2023-12-04 16:04:38 +0100

    Update cell.h

--- cell.h
+++ cell.h
@@ -9,7 +9,7 @@
 typedef struct Cell {
     int value;           // Value of the cell
     int numLevels;       // Number of levels in the cell
-    struct Cell** levels; // Array of pointers to cells at different levels
+    struct Cell* levels[]; // Array of pointers to cells at different levels
 } Cell;
 
 // Function to create a cell
