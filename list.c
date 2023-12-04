77a9831ea7af48c9742d1128057ab1c2dd0847e3
FLEURYESTEBAN <128791669+FLEURYESTEBAN@users.noreply.github.com>
2023-12-04 16:05:00 +0100

    Update list.c

--- list.c
+++ list.c
@@ -6,39 +6,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include "list.h"
+#include <math.h>
 
 // Function to create a cell
 Cell* createCell(int value, int numLevels) {
     // Allocate memory for the cell structure
-    Cell* newCell = (Cell*)malloc(sizeof(Cell));
 
-    // Check if memory allocation was successful
-    if (newCell == NULL) {
-        // Handle memory allocation failure
-        return NULL;
-    }
+    Cell* newcell = malloc(sizeof(Cell*)+ sizeof(Cell**) *numLevels);
 
     // Set the value and number of levels for the cell
-    newCell->value = value;
-    newCell->numLevels = numLevels;
+    newcell->value = value;
+    newcell->numLevels = numLevels;
 
-    // Allocate memory for the array of pointers to cells at different levels
-    newCell->levels = (Cell**)malloc(numLevels * sizeof(Cell*));
-
-    // Check if memory allocation was successful
-    if (newCell->levels == NULL) {
-        // Handle memory allocation failure
-        free(newCell); // Free the memory allocated for the cell structure
-        return NULL;
-    }
 
     // Initialize the array of pointers to NULL
     for (int i = 0; i < numLevels; ++i) {
-        newCell->levels[i] = NULL;
+        newcell->levels[i] = NULL;
     }
 
     // Return the pointer to the created cell
-    return newCell;
+    return newcell;
 }
 
 // Function to create an empty list with the given maximum number of levels
@@ -46,43 +33,76 @@ List* createList(int maxLevels) {
     // Allocate memory for the list structure
     List* newList = (List*)malloc(sizeof(List));
 
-    // Check if memory allocation was successful
-    if (newList == NULL) {
-        // Handle memory allocation failure
-        return NULL;
-    }
+
 
     // Set the maximum number of levels for the list
     newList->maxLevels = maxLevels;
-
+    for (int i = 0; i < maxLevels; i++)
     // Set the head of the list to NULL (empty list)
-    newList->head = NULL;
+        newList->head[i] = NULL;
 
     // Return the pointer to the created list
     return newList;
 }
 
 // Function to insert a cell with levels at the beginning of the list
-void insertCellAtHead(List* list, Cell* cell) {
-    // Set the levels of the cell to match the maximum levels of the list
-    int levelsToSet = (cell->numLevels > list->maxLevels) ? list->maxLevels : cell->numLevels;
-    cell->numLevels = levelsToSet;
+void insertSorted(List* list, Cell* cell) {
+    int level = cell->numLevels - 1;
 
-    // Link the cell to the current head of the list
-    for (int i = 0; i < levelsToSet; ++i) {
-        cell->levels[i] = list->head;
+    // If the list is empty, or the new cell value is smaller than the head value
+    if (list->head[level] == NULL || cell->value < list->head[level]->value) {
+        cell->levels[level] = list->head[level];
+        list->head[level] = cell;
+    } else {
+        // Find the position to insert the cell
+        Cell* current = list->head[level];
+        Cell* previous = NULL;
+        while (current != NULL && current->value < cell->value) {
+            previous = current;
+            current = current->levels[level];
+        }
+
+        // Insert the cell at the correct position
+        cell->levels[level] = current;
+        if (previous != NULL) {
+            previous->levels[level] = cell;
+        } else {
+            // If previous is NULL, it means the cell should be the new head
+            list->head[level] = cell;
+        }
     }
 
-    // Set the head of the list to the new cell
-    list->head = cell;
-}
+    // Insert the cell in lower levels
+    for (int i = level - 1; i >= 0; --i) {
+        if (list->head[i] == NULL || cell->value < list->head[i]->value) {
+            cell->levels[i] = list->head[i];
+            list->head[i] = cell;
+        } else {
+            // Find the position to insert the cell in lower levels
+            Cell* current = list->head[i];
+            Cell* previous = NULL;
+            while (current != NULL && current->value < cell->value) {
+                previous = current;
+                current = current->levels[i];
+            }
 
+            // Insert the cell at the correct position in lower levels
+            cell->levels[i] = current;
+            if (previous != NULL) {
+                previous->levels[i] = cell;
+            } else {
+                // If previous is NULL, it means the cell should be the new head
+                list->head[i] = cell;
+            }
+        }
+    }
+}
 // Function to display all cells at a specific level in the list
 void displayCellsAtLevel(List* list, int level) {
     printf("Level %d: [ list head_%d @-]-->", level,level);
 
     // Traverse the list and display cells at the specified level
-    Cell* current = list->head;
+    Cell* current = list->head[level];
     while (current != NULL) {
         if (level < current->numLevels) {
             printf("[ %d|@-]-->", current->value);
@@ -92,12 +112,19 @@ void displayCellsAtLevel(List* list, int level) {
 
     printf("NULL\n");
 }
-
+void displayListByLevel(List* list, int level) {
+    Cell* current = list->head[level];
+    while (current != NULL) {
+        printf("%d ", current->value);
+        current = current->levels[level];
+    }
+    printf("\n");
+}
 // Function to display all levels of the list
 void displayAllLevels(List* list) {
-    // Display cells at each level
-    for (int i = 0; i < list->maxLevels; ++i) {
-        displayCellsAtLevel(list, i);
+    for (int i = list->maxLevels - 1; i >= 0; --i) {
+        printf("Level %d: ", i);
+        displayListByLevel(list, i);
     }
 }
 
@@ -110,7 +137,7 @@ void displayAllLevelsAligned(List* list) {
         printf("Level %d: [ list head_%d @-]--", i,i);
 
         // Traverse the list and display cells at the specified level
-        Cell* current = list->head;
+        Cell* current = list->head[i];
         while (current != NULL) {
             if (i < current->numLevels) {
                 printf(">[ %d|@-]--", current->value);
@@ -125,36 +152,53 @@ void displayAllLevelsAligned(List* list) {
 }
 
 // Function to insert a cell at the correct position to keep the list sorted
-void insertCellSorted(List* list, Cell* cell) {
-    int levelsToSet = (cell->numLevels > list->maxLevels) ? list->maxLevels : cell->numLevels;
-    cell->numLevels = levelsToSet;
 
-    // Special case: If the list is empty or the new cell should be the new head
-    if (list->head == NULL || cell->value < list->head->value) {
-        for (int i = 0; i < levelsToSet; ++i) {
-            cell->levels[i] = list->head;
+
+List* MakeBigList(int n) {
+    int cellCount = (int)pow(2, n) - 1;
+    int middle = (int)(cellCount/2);
+    int level = 1;
+    List* newList = createList(n);
+
+    for (int i = 1; i <= cellCount; ++i) {
+        Cell* cell;
+        if (i % 2 == 0) {
+            level += i > middle + 1? -1 : 1;
+            cell = createCell(i, level);
         }
-        list->head = cell;
-        return;
+        else {
+            cell = createCell(i, 1); // Si le chiffre est impair le niveau est 1
+        }
+        insertSorted(newList, cell);
     }
 
-    // Traverse the list to find the correct position to insert the cell
-    Cell* current = list->head;
-    Cell* previous = NULL;
-
-    while (current != NULL && current->value < cell->value) {
-        previous = current;
-        current = current->levels[0];
-    }
-
-    // Insert the cell at the correct position
-    for (int i = 0; i < levelsToSet; ++i) {
-        cell->levels[i] = current;
-    }
-
-    if (previous != NULL) {
-        previous->levels[0] = cell;
-    }
+    return newList;
 }
+Cell* search(List* list, int value){
+    int level = list->maxLevels - 1;
+    Cell* next = list->head[level];
+    Cell* lastInferior = next;
 
+    for (; level >= 0; --level) {
+        if (lastInferior->value <= value) {
+            while (next != NULL) {
+                if (next->value == value) return next;
+                if (next->value > value) break;
+
+                lastInferior = next;
+                next = next->levels[level];
+            }
+
+            next = lastInferior;
+        }
+        else {
+            next = list->head[level];
+            lastInferior = next;
+        }
+    }
+
+    return NULL;
+
+
+}
 
